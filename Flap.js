7c4632867bfafb984a9efc3df54a0329f9df7139
JasperTresidder <51917264+JasperTresidder@users.noreply.github.com>
2019-06-18 03:31:19 +0100

    Update Flap.js

--- Flap.js
+++ Flap.js
@@ -1,22 +1,26 @@
 ball = [];
 temp = [];
-
+var alpha, beta, gamma;
 var oW = 0;
 var oH = 0;
 
 function setup() {
-  createCanvas(displayWidth,displayHeight);
+  createCanvas(displayWidth, displayHeight);
   oW = windowWidth;
   oH = windowHeight;
   pipe = new Pipe();
-  
+
   button = createButton('fullScreen');
   button.position(3, 3);
   button.mousePressed(ESC);
-  
+
   button = createButton('reset');
   button.position(width - 45, 3);
   button.mousePressed(REDO);
+  
+  alphas = 670;
+  beta = 0;
+  gamma = 0;
 }
 
 function draw() {
@@ -39,8 +43,8 @@ function draw() {
 class Pipe {
   constructor() {
     this.x = width - 86;
-    this.y = height - width/2;
-    this.speed = createVector(4,0);
+    this.y = height - width / 2;
+    this.speed = createVector(4, 0);
 
   }
 
@@ -63,59 +67,64 @@ class Ball {
   constructor() {
     this.x = mouseX;
     this.y = mouseY;
-    this.velocity = createVector(0,0)
+    this.velocity = createVector(0, 0);
     this.mass = 1;
-    this.g = 1;
+    this.g = createVector(0, 1);
     this.r = 50;
     this.sidea = 0;
     this.down = 0;
     this.angle = 0;
-    this.touches = function(other){
-      var d = sqrt((this.x - other.x)*(this.x - other.x) + (this.y - other.y)*(this.y - other.y))
-      if(d < 50){
+    this.grav = createVector(0, 0);
+    this.touches = function(other) {
+      var d = sqrt((this.x - other.x) * (this.x - other.x) + (this.y - other.y) * (this.y - other.y))
+      if (d < 50) {
         return true;
-      }else{
+      } else {
         return false;
       }
-      
+
     }
   }
-    
-  
+
+
   update() {
-    this.velocity.y += this.g;
+    this.grav = createVector(beta,alphas);
+    this.g = createVector(0,1);
+    
+    this.velocity.y += this.g.y;
     this.y += this.velocity.y;
     this.velocity.x += this.sidea;
+    this.velocity.x += this.g.x;
     this.x += this.velocity.x;
-    
-    
-    if(this.x +25 > width || this.x - 25 < 0){
-      this.velocity.x = - this.velocity.x;
-      if (this.x + 25 > width){
+
+
+    if (this.x + 25 > width || this.x - 25 < 0) {
+      this.velocity.x = -this.velocity.x;
+      if (this.x + 25 > width) {
         this.x = width - 25;
-      }else{
+      } else {
         this.x = 25;
       }
-      
-    }    
+
+    }
     if (this.y + 25 > height) {
-      this.velocity.y = -(this.velocity.y );
+      this.velocity.y = -(this.velocity.y);
       this.y = -25 + height;
-      if(this.down == 0){
+      if (this.down == 0) {
         //this.velocity.x = random(-4, 4)
       }
       this.down += 1;
     }
-    
-    for(var i = 0; i < ball.length; i++){
-      if(this.touches(ball[i])){
-        resolveCollision(this,ball[i]);
+
+    for (var i = 0; i < ball.length; i++) {
+      if (this.touches(ball[i])) {
+        resolveCollision(this, ball[i]);
       }
-    //console.log('ok');
-    
+      //console.log('ok');
+
     }
-    this.velocity.x = this.velocity.x*0.993;
-    this.velocity.y = this.velocity.y*0.993;
+    this.velocity.x = this.velocity.x * 0.993;
+    this.velocity.y = this.velocity.y * 0.993;
   }
 
   show() {
@@ -127,63 +136,77 @@ class Ball {
 }
 
 function mouseClicked() {
-  if((mouseX > 80 || mouseY > 23) &&(mouseX<width-50 || mouseY > 23)){
+  if ((mouseX > 80 || mouseY > 23) && (mouseX < width - 50 || mouseY > 23)) {
     ball.push(new Ball());
   }
 }
-function ESC(){
-    let fs = fullscreen();
-    fullscreen(!fs);
+
+function ESC() {
+  let fs = fullscreen();
+  fullscreen(!fs);
 }
 
 
 function resolveCollision(particle, otherParticle) {
-    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+  const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+  const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
 
-    const xDist = otherParticle.x - particle.x;
-    const yDist = otherParticle.y - particle.y;
+  const xDist = otherParticle.x - particle.x;
+  const yDist = otherParticle.y - particle.y;
 
-    // Prevent accidental overlap of particles
-    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+  // Prevent accidental overlap of particles
+  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
 
-        // Grab angle between the two colliding particles
-        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+    // Grab angle between the two colliding particles
+    const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
 
-        // Store mass in var for better readability in collision equation
-        const m1 = particle.mass;
-        const m2 = otherParticle.mass;
+    // Store mass in var for better readability in collision equation
+    const m1 = particle.mass;
+    const m2 = otherParticle.mass;
 
-        // Velocity before equation
-        const u1 = rotates(particle.velocity, angle);
-        const u2 = rotates(otherParticle.velocity, angle);
+    // Velocity before equation
+    const u1 = rotates(particle.velocity, angle);
+    const u2 = rotates(otherParticle.velocity, angle);
 
-        // Velocity after 1d collision equation
-        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
-        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+    // Velocity after 1d collision equation
+    const v1 = {
+      x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2),
+      y: u1.y
+    };
+    const v2 = {
+      x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2),
+      y: u2.y
+    };
 
-        // Final velocity after rotating axis back to original location
-        const vFinal1 = rotates(v1, -angle);
-        const vFinal2 = rotates(v2, -angle);
+    // Final velocity after rotating axis back to original location
+    const vFinal1 = rotates(v1, -angle);
+    const vFinal2 = rotates(v2, -angle);
 
-        // Swap particle velocities for realistic bounce effect
-        particle.velocity.x = vFinal1.x;
-        particle.velocity.y = vFinal1.y;
+    // Swap particle velocities for realistic bounce effect
+    particle.velocity.x = vFinal1.x;
+    particle.velocity.y = vFinal1.y;
 
-        otherParticle.velocity.x = vFinal2.x;
-        otherParticle.velocity.y = vFinal2.y;
-    }
+    otherParticle.velocity.x = vFinal2.x;
+    otherParticle.velocity.y = vFinal2.y;
+  }
 }
 
 function rotates(velocity, angle) {
-    const rotatedVelocities = {
-        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-    };
+  const rotatedVelocities = {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+  };
 
-    return rotatedVelocities;
+  return rotatedVelocities;
 }
 
-function REDO(){
+function REDO() {
   ball = [];
 }
+
+window.addEventListener('deviceorientation', function(e) 
+{
+  alphas = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
